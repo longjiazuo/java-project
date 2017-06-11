266810cc64aa3827dc47423332abe89e1f6dee18
longjiazuo <812198178@qq.com>
2017-06-11 19:12:01 +0800

    增加线程通信示例代码

---
+++ java-thread/src/main/java/org/light4j/thread/communicate/DaemonThread.java
@@ -0,0 +1,28 @@
+package org.light4j.thread.communicate;
+
+/**
+ * 守护线程(后台线程，精力线程)测试
+ * 
+ * @author longjiazuo
+ * 
+ */
+public class DaemonThread extends Thread {
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 1000; i++) {
+			System.out.println(this.getName() + " " + i);
+		}
+	}
+
+	public static void main(String[] args) {
+		DaemonThread dt = new DaemonThread();
+		// 将线程设置为守护线程
+		dt.setDaemon(true);
+		// 启动守护线程
+		dt.start();
+		for (int i = 0; i < 10; i++) {
+			System.out.println(Thread.currentThread().getName() + " " + i);
+		}
+	}
+}
---
+++ java-thread/src/main/java/org/light4j/thread/communicate/PriorityTest.java
@@ -0,0 +1,44 @@
+package org.light4j.thread.communicate;
+
+/**
+ * 线程优先级Priority使用
+ * 
+ * @author longjiazuo
+ * 
+ */
+public class PriorityTest extends Thread {
+
+	public PriorityTest(String name) {
+		super(name);
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 50; i++) {
+			System.out.println(getName() + ",其优先级是：" + getPriority()
+					+ ",循环变量的值是：" + i);
+		}
+	}
+
+	public static void main(String[] args) {
+		// 改变主线程的优先级
+		Thread.currentThread().setPriority(6);
+		for (int i = 0; i < 30; i++) {
+			if (i == 10) {
+				PriorityTest low = new PriorityTest("低级");
+				low.start();
+				System.out.println("低级创建之初的线程优先级是：" + low.getPriority());
+				// 将该线程设置为最低优先级
+				low.setPriority(Thread.MIN_PRIORITY);
+			}
+
+			if (i == 20) {
+				PriorityTest high = new PriorityTest("高级");
+				high.start();
+				System.out.println("高级创建之初的线程优先级是：" + high.getPriority());
+				// 将该线程设置为最高优先级
+				high.setPriority(Thread.MAX_PRIORITY);
+			}
+		}
+	}
+}
