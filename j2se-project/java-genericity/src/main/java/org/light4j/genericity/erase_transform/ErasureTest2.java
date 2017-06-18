fe90bfbc6a092807a2001514d46210cc7cdd817b
longjiazuo <812198178@qq.com>
2017-06-18 21:53:54 +0800

    增加擦除和转换示例代码

---
+++ j2se-project/java-genericity/src/main/java/org/light4j/genericity/erase_transform/ErasureTest2.java
@@ -0,0 +1,16 @@
+package org.light4j.genericity.erase_transform;
+
+import java.util.*;
+
+public class ErasureTest2 {
+	public static void main(String[] args) {
+		List<Integer> li = new ArrayList<>();
+		li.add(6);
+		li.add(9);
+		List list = li;
+		// 下面代码引起“未经检查的转换”的警告，编译、运行时完全正常
+		List<String> ls = list; // ①
+		// 但只要访问ls里的元素，如下面代码将引起运行时异常。
+		System.out.println(ls.get(0));
+	}
+}
